e6d1370750c8ec4f5f0df03dac4cf2db2216f5db
kunal2762 <kunalrahangdale2762@gmail.com>
2024-12-06 09:59:11 +0530

    Add files via upload

---
+++ script-lp.js
@@ -0,0 +1,23 @@
+var index = 0;
+fungsi();
+
+function fungsi() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  index++;
+  if (index > slides.length) {
+    index = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[index - 1].style.display = "block";
+  //   dots[index - 1].className += "active";
+
+  setTimeout(fungsi, 1500);
+}
